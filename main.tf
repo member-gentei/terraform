be63a2208b9e32f389e1b9e7fddc3b227f628537
Mark Ignacio <mark@ignacio.io>
2020-12-27 07:35:19 -0800

    converted IAM stuff to Terraform

---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "google" {
+  project = "member-gentei"
+}
+
+resource "google_service_account" "api" {
+  account_id   = "gentei-api"
+  display_name = "gentei-api"
+}
+
+resource "google_service_account" "member-check" {
+  account_id   = "gentei-member-check"
+  display_name = "gentei-member-check"
+  description  = "Performs membership checks (via systemd.timer)"
+}
+
+resource "google_service_account" "refresh-data" {
+  account_id   = "gentei-refresh-data"
+  display_name = "gentei-refresh-data"
+}
+
+resource "google_service_account" "bot" {
+  account_id   = "gentei-bot"
+  display_name = "gentei-bot"
+}
+
+resource "google_project_iam_member" "logging_logWriter" {
+  for_each = toset([for sa in [
+    google_service_account.bot,
+    google_service_account.member-check,
+    google_service_account.refresh-data,
+  ] : sa.email])
+  role   = "roles/logging.logWriter"
+  member = "serviceAccount:${each.key}"
+}
+
+resource "google_project_iam_member" "datastore_user" {
+  for_each = toset([for sa in [
+    google_service_account.api,
+    google_service_account.bot,
+    google_service_account.member-check,
+    google_service_account.refresh-data,
+  ] : sa.email])
+  role   = "roles/datastore.user"
+  member = "serviceAccount:${each.key}"
+}
