f86ff339da368cb3d2ef560c696d18c075425d93
Mark Ignacio <mark@ignacio.io>
2024-06-23 13:18:53 -0700

    feat: alerting stuff

--- gcp_alerting.tf
+++ gcp_alerting.tf
@@ -3,13 +3,14 @@ locals {
   monitoring_sa = "service-${data.google_project.current.number}@gcp-sa-monitoring-notification.iam.gserviceaccount.com"
 }
 
+
 resource "google_pubsub_topic" "alerting" {
   name = "alerting"
 }
 
 resource "google_monitoring_notification_channel" "pubsub" {
   display_name = "Alerting Channel"
-  type = "pubsub"
+  type         = "pubsub"
   labels = {
     topic = google_pubsub_topic.alerting.id
   }
@@ -17,8 +18,63 @@ resource "google_monitoring_notification_channel" "pubsub" {
 
 resource "google_pubsub_topic_iam_binding" "publisher" {
   topic = google_pubsub_topic.alerting.name
-  role = "roles/pubsub.publisher"
+  role  = "roles/pubsub.publisher"
   members = [
     "serviceAccount:${local.monitoring_sa}"
   ]
+}
+
+resource "google_service_account" "alerting-discord" {
+  account_id = "alerting-discord"
+}
+
+resource "google_cloud_run_service_iam_binding" "alerting-discord" {
+  role     = "roles/run.invoker"
+  location = "us-central1"
+  service  = google_cloudfunctions2_function.discord-alert.name
+  members = [
+    "serviceAccount:${google_service_account.alerting-discord.email}"
+  ]
+}
+
+resource "google_secret_manager_secret_iam_binding" "alerting-discord" {
+  role      = "roles/secretmanager.secretAccessor"
+  secret_id = data.google_secret_manager_secret.alerting-discord-webhook-url.secret_id
+  members = [
+    "serviceAccount:${google_service_account.alerting-discord.email}"
+  ]
+}
+
+resource "google_cloudfunctions2_function" "discord-alert" {
+  name     = "discord-alert"
+  location = "us-central1"
+
+  build_config {
+    entry_point = "HandlePubSubAlert"
+    runtime     = "go122"
+    # gcloud invocations mutate the source {} block
+  }
+  service_config {
+    all_traffic_on_latest_revision = true
+    available_cpu                  = "83m"
+    available_memory               = "128Mi"
+    service_account_email          = google_service_account.alerting-discord.email
+    secret_environment_variables {
+      key        = "DISCORD_WEBHOOK_URL"
+      project_id = data.google_project.current.number
+      secret     = data.google_secret_manager_secret.alerting-discord-webhook-url.secret_id
+      version    = "latest"
+    }
+  }
+  event_trigger {
+    event_type   = "google.cloud.pubsub.topic.v1.messagePublished"
+    retry_policy = "RETRY_POLICY_DO_NOT_RETRY"
+  }
+  lifecycle {
+    ignore_changes = [build_config[0].source]
+  }
+}
+
+data "google_secret_manager_secret" "alerting-discord-webhook-url" {
+  secret_id = "alerting-discord-webhook-url"
 }
--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     }
     google = {
       source  = "hashicorp/google"
-      version = "4.6.0"
+      version = "5.34.0"
     }
   }
 }
